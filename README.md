1d6db7c98f6408ca2a3ddda6200cb5b4e5a365cf
shubhamsingh333 <shubhamsingh333333@gmail.com>
2021-05-08 21:59:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-e6mjjy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-e6mjjy)
---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const App = () => {
+  const [fullName, setfullName] = useState({
+    fname: '',
+    lname: '',
+  });
+
+  const InputEvent = event => {
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setfullName(prepValue => {
+      // console.log(prepValue);
+      if (name === "fName") {
+        return {
+          fname: value,
+          lname: prepValue.lname
+        };
+      }
+      else
+        if (name === "lName") {
+        return {
+          fname: prepValue.fname,
+          lname: value
+        };
+      
+
+      }
+    });
+  };
+
+  const onSubmits = event => {
+    event.preventDefault();
+    alert('form submitted');
+  };
+
+  return (
+    <>
+      <div className="main_div">
+        <form onSubmit={onSubmits}>
+          <h1>
+            Hello {fullName.fname} 
+            {fullName.lname}
+          </h1>
+          <input
+            type="text"
+            placeholder="Enter your Name"
+            name="fName"
+            onChange={InputEvent}
+            value={fullName.fname}
+          />
+          <br />
+          <br/>
+          <input
+            type="text"
+            placeholder="Enter your lName"
+            name="lName"
+            onChange={InputEvent}
+            value={fullName.lname}
+          />
+
+          <br />
+          <br/>
+          <input
+            type="number"
+            placeholder="Enter your phone"
+            name="phone"
+            onChange={InputEvent}
+            value={fullName.phone}
+          />
+          <br />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+            onChange={InputEvent}
+            value={fullName.email}
+          />
+          <button type="submit"> Submit me </button>
+        </form>
+      </div>
+    </>
+  );
+};
+export default App;
